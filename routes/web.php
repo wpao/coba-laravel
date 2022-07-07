18e9b5e0636c9af1e0a7009a6613cf6394161614
Ahmad31052021 <85085243+Ahmad31052021@users.noreply.github.com>
2022-07-07 14:26:15 +0800

    video 3 view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/posts', function () {
+    return view('posts');
 });
