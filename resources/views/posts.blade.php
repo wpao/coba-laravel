0291fb6c6d25518473de337c2f5389ec0d9514de
Ahmad31052021 <85085243+Ahmad31052021@users.noreply.github.com>
2022-07-14 09:04:06 +0800

    v12 melengkapi tampilan categories.blade.php menu personal masih kosong

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,8 @@
 <div class="card mb-3">
     {{-- curl https://api.unsplash.com/users/samuelzeller/photos --}}
     {{-- <img src="https://api.unsplash.com/users/samuelzeller/photos?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
-    {{-- <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
-    <img src="..." class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    {{-- <img src="..." class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
     <div class="card-body text-center">
         <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
         <p>
@@ -31,8 +31,8 @@
         <div class="col-md-4 mb-3">
             <div class="card">
                 <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-                {{-- <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}"> --}}
-                <img src="..." class="card-img-top" alt="{{ $post->category->name }}">
+                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                {{-- <img src="..." class="card-img-top" alt="{{ $post->category->name }}"> --}}
                 <div class="card-body">
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p>
