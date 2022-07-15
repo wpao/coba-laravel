bcb9b9c69a9469b4a29ac28ec5f03c67a8bd539b
paozanwa22 <fausanwadi@gmail.com>
2022-07-15 08:58:35 +0800

    v13 serching tapi masih dalam route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,13 +9,21 @@ class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::latest();
+
+        if (request()->has('search')) {
+            $posts = $posts->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
         return view('posts', [
             "title" => "All Posts",
             "active" => "posts",
             // "posts" => Post::all()
             // "posts" => Post::latest()->get()
             // "posts" => Post::with(['author', 'category'])->latest()->get()
-            "posts" => Post::latest()->get()
+            // "posts" => Post::latest()->get()
+            "posts" => $posts->get()
         ]);
     }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,50 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row justify-content-center mb-3">
+    <div class="col-md-6">
+        <form action="/posts">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+                <button class="btn btn-danger" type="submit">Search</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @if($posts->count())
 <div class="card mb-3">
     {{-- curl https://api.unsplash.com/users/samuelzeller/photos --}}
     {{-- <img src="https://api.unsplash.com/users/samuelzeller/photos?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
-    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
-    {{-- <img src="..." class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
+    {{-- <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}"> --}}
+    <img src="..." class="card-img-top" alt="{{ $posts[0]->category->name }}">
     <div class="card-body text-center">
         <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
         <p>
@@ -21,9 +57,6 @@
         <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read More</a>
     </div>
 </div>
-@else
-<p class="text-center fs-4">No post found.</p>
-@endif
 
 <div class="container">
     <div class="row">
@@ -31,8 +64,8 @@
         <div class="col-md-4 mb-3">
             <div class="card">
                 <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-                {{-- <img src="..." class="card-img-top" alt="{{ $post->category->name }}"> --}}
+                {{-- <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}"> --}}
+                <img src="..." class="card-img-top" alt="{{ $post->category->name }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p>
@@ -48,5 +81,8 @@
         @endforeach
     </div>
 </div>
+@else
+<p class="text-center fs-4">No post found.</p>
+@endif
 
 @endsection
